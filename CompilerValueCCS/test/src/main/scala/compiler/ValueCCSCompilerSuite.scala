76e237e264e28ef6139596223dd2bf3781e21cb3
Alessandro Flori <alessandro.flori@proton.me>
2022-07-31 14:21:19 +0200

    Tests updated

--- CompilerValueCCS/test/src/main/scala/compiler/ValueCCSCompilerSuite.scala
+++ CompilerValueCCS/test/src/main/scala/compiler/ValueCCSCompilerSuite.scala
@@ -1,17 +1,36 @@
 package main.scala.compiler
 
-import main.scala.parser.ValueCCSLexer
-import main.scala.parser.ValueCCSParser
+import main.scala.parser._
+import main.scala.ast._
+import main.scala.ast.CommonAst._
+import main.scala.ast.Aexpr._
+import main.scala.ast.Aexpr.Factor._
+
+import org.antlr.v4.runtime._
+
+object TestParser:
+
+  def apply(input: String): (CcsvpParser, ValueCCSParser.type) =
+    val lexer = new CcsvpLexer(CharStreams.fromString(input))
+    val tokens = new CommonTokenStream(lexer)
+    val parser = new CcsvpParser(tokens)
+
+    val ccsvpVisitor = ValueCCSParser
+    (parser, ccsvpVisitor)
 
 class ValueCCSCompilerSuite extends munit.FunSuite:
+  
   test("Program: K(x) = 'kr(x).K + kw(y).K(y)") {
-    val ast = for {
-      tokens <- ValueCCSLexer("K(x) = 'kr(x).K + kw(y).K(y)")
-      ast <- ValueCCSParser(tokens, ValueCCSParser.program)
-    } yield ast
-    ast match {
-      case Right(valid) => 
-        println(valid); println(PureCCSCompiler(valid, 1, 2).mkString("\n"))
-      case Left(notValid) => fail(s"Could not parse grammar: {notValid}")
-    }
+    val progr = "K(x) = 'kr(x).K + kw(y).K(y)"
+    val (parser, ccsvpVisitor) = TestParser(progr)
+    assertEquals(ccsvpVisitor.visit(parser.program),
+      ValueCCSProgram(Constant("K", Some(List(Variable("x")))),
+      Sum(List(
+        OutputCh(
+          Channel("kr"),
+          Some(Expr(Term(ID(Variable("x")), Nil), Nil)),
+          Const("K", None)
+        ),
+        InputCh(Channel("kw"),Some(Variable("y")),Const("K",Some(List(Expr(Term(ID(Variable("y")),Nil),Nil)))))
+      ))))
   }
--- CompilerValueCCS/test/src/main/scala/parser/ValueCCSParserSuite.scala.scala
+++ CompilerValueCCS/test/src/main/scala/parser/ValueCCSParserSuite.scala.scala
@@ -7,21 +7,24 @@ import main.scala.ast.Bexpr._
 import main.scala.ast.Bexpr.BoolOperator._
 import main.scala.ast.Bexpr.BoolTerm._
 import main.scala.ast.CommonAst._
-import main.scala.ast.ValueCCS._
 import main.scala.eval.EvalAexpr.eval as evalA
 import main.scala.eval.EvalBexpr.eval as evalB
 import main.scala.parser._
-import main.scala.parser.ValueCCSParser._
+
+import org.antlr.v4.runtime._
+import main.scala.compiler.TestParser
 
 class ValueCCSParserSuite extends munit.FunSuite {
-  test("aexpr") {
 
-    val ast = for {
-      tokens <- ValueCCSLexer("5+24*3-4")
-      ast <- ValueCCSParser(tokens, phrase(ValueCCSParser.aexpr))
-    } yield ast
+  def returnCcsvpAst(str: String): ValueCCS =
+    val (parser, ccsvpVisitor) = TestParser(str)
+    ccsvpVisitor.visit(parser.ccsvp)
+
+  test("aexpr") {
+    val (parser, ccsvpVisitor) = TestParser("5+24*3-4")
+    val ast = ccsvpVisitor.visit(parser.expr)
     assertEquals(
-      ast.getOrElse("Mistake"),
+      ast,
       Expr(
         Term(NUMBER(Natural(5)), List.empty),
         List(
@@ -33,13 +36,10 @@ class ValueCCSParserSuite extends munit.FunSuite {
   }
 
   test("constant") {
-    val ast = for {
-      tokens <- ValueCCSLexer("Kr(x)")
-      ast <- ValueCCSParser(tokens, ValueCCSParser.constant)
-    } yield ast
+    val ast = returnCcsvpAst("Kr(x)")
     assertEquals(
-      ast.getOrElse(""),
-      Constant(
+      ast,
+      Const(
         "Kr",
         Some(List(Expr(Term(Factor.ID(Variable("x")), List.empty), List.empty)))
       )
@@ -47,49 +47,37 @@ class ValueCCSParserSuite extends munit.FunSuite {
   }
 
   test("inputch") {
-    val ast = for {
-      tokens <- ValueCCSLexer("kr(x).K")
-      ast <- ValueCCSParser(tokens, ValueCCSParser.inputCh)
-    } yield ast
+    val ast = returnCcsvpAst("kr(x).K")
     assertEquals(
-      ast.getOrElse("Error"),
-      InputCh(Channel("kr"), Some(Variable("x")), Constant("K", None))
+      ast,
+      InputCh(Channel("kr"), Some(Variable("x")), Const("K", None))
     )
   }
 
   test("tauch") {
-    val ast = for {
-      tokens <- ValueCCSLexer("tau.K")
-      ast <- ValueCCSParser(tokens, ValueCCSParser.tauCh)
-    } yield ast
+    val ast = returnCcsvpAst("tau.K")
     assertEquals(
-      ast.getOrElse("Error"),
-      TauCh(Constant("K", None))
+      ast,
+      TauCh(Const("K", None))
     )
   }
 
   test("outputch") {
-    val ast = for {
-      tokens <- ValueCCSLexer("'kr(1).K")
-      ast <- ValueCCSParser(tokens, phrase(ValueCCSParser.outputCh))
-    } yield ast
+    val ast = returnCcsvpAst("'kr(1).K")
     assertEquals(
-      ast.getOrElse("Error"),
+      ast,
       OutputCh(
         Channel("kr"),
         Some(Expr(Term(Factor.NUMBER(Natural(1)), List.empty), List.empty)),
-        Constant("K", None)
+        Const("K", None)
       )
     )
   }
 
   test("ifthen") {
-    val ast = for {
-      tokens <- ValueCCSLexer("if 1 > 2 then K")
-      ast <- ValueCCSParser(tokens, ValueCCSParser.ifThen)
-    } yield ast
+    val ast = returnCcsvpAst("if 1 > 2 then K")
     assertEquals(
-      ast.getOrElse("Error"),
+      ast,
       IfThen(
         BoolBinOp(
           BoolExpr(
@@ -101,58 +89,46 @@ class ValueCCSParserSuite extends munit.FunSuite {
           ),
           Nil
         ),
-        Constant("K", None)
+        Const("K", None)
       )
     )
   }
 
   test("par") {
-    val ast = for {
-      tokens <- ValueCCSLexer("K | K")
-      ast <- ValueCCSParser(tokens, ValueCCSParser.par)
-    } yield ast
+    val ast = returnCcsvpAst("K | K")
     assertEquals(
-      ast.getOrElse("Error"),
-      Par(Constant("K", None), Constant("K", None))
+      ast,
+      Par(Const("K", None), Const("K", None))
     )
   }
 
   test("sum") {
-    val ast = for {
-      tokens <- ValueCCSLexer("K + K")
-      ast <- ValueCCSParser(tokens, ValueCCSParser.sum)
-    } yield ast
+    val ast = returnCcsvpAst("K + K")
     assertEquals(
-      ast.getOrElse("Error"),
-      Sum(List(Constant("K", None), Constant("K", None)))
+      ast,
+      Sum(List(Const("K", None), Const("K", None)))
     )
   }
 
   test("restrict") {
-    val ast = for {
-      tokens <- ValueCCSLexer("K \\ {y}")
-      ast <- ValueCCSParser(tokens, ValueCCSParser.restrict)
-    } yield ast
+    val ast = returnCcsvpAst("K \\ {y}")
     assertEquals(
-      ast.getOrElse("Error"),
-      Restrict(Constant("K", None), List(Channel("y")))
+      ast,
+      Restrict(Const("K", None), List(Channel("y")))
     )
   }
 
   test("valueCCS") {
-    val ast = for {
-      tokens <- ValueCCSLexer("'kr(x).K|kw(y).K(y)")
-      ast <- ValueCCSParser(tokens, ValueCCSParser.valueCCS)
-    } yield ast
+    val ast = returnCcsvpAst("'kr(x).K|kw(y).K(y)")
     assertEquals(
-      ast.getOrElse("Error"),
+      ast,
       Par(
         OutputCh(
           Channel("kr"),
           Some(Expr(Term(ID(Variable("x")), Nil), Nil)),
-          Constant("K", None)
+          Const("K", None)
         ),
-        InputCh(Channel("kw"),Some(Variable("y")),Constant("K",Some(List(Expr(Term(ID(Variable("y")),Nil),Nil)))))
+        InputCh(Channel("kw"),Some(Variable("y")),Const("K",Some(List(Expr(Term(ID(Variable("y")),Nil),Nil)))))
       )
     )
   }
